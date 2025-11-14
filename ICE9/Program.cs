38c9a289ab8834bc2ce2ebe07087e554ca7a9a3a
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2025-11-14 08:44:27 -0500

    Pre-loaded forms and created Form List in Program class

--- ICE9/Program.cs
+++ ICE9/Program.cs
@@ -12,24 +12,43 @@ namespace ICE9
 
     internal static class Program
     {
+        // Declaring Form Variables
         public static SplashForm SplashForm;
         public static SelectionForm SelectionForm;
         public static NextForm NextForm;
         public static FinalForm FinalForm;
         public static AboutForm AboutForm;
 
+        // Declaring a List of Type Form
         public static List<Form> Forms;
 
+        // Declaring a bool that will help us exit
         public static bool IsExiting = false;
 
 
         [STAThread]
         static void Main()
         {
-
             ApplicationConfiguration.Initialize();
 
-            // Creates a new Instance of SelectionForm
+            // Initializing the Form Variables
+            // by instantiating objects of the related form types
+            SplashForm = new SplashForm();
+            SelectionForm = new SelectionForm();
+            NextForm = new NextForm();
+            FinalForm = new FinalForm();
+            AboutForm = new AboutForm();
+
+            // Initializing the Forms List
+            Forms =
+            [
+                SplashForm,
+                SelectionForm,
+                NextForm,
+                FinalForm,
+                AboutForm
+            ];
+
             Application.Run(new SelectionForm());
         }
     }
