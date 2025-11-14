f3a8c589330376a17374a138c1d30d0732d44115
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2025-11-14 09:24:15 -0500

    configured SplashForm

--- ICE9/SplashForm.cs
+++ ICE9/SplashForm.cs
@@ -10,11 +10,25 @@ using System.Windows.Forms;
 
 namespace ICE9
 {
+
     public partial class SplashForm : Form
     {
         public SplashForm()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// This event is triggered when the splash timer ticks. It stops the timer,
+        /// hides the splash form, and shows the selection form.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SplashTimer_Tick(object sender, EventArgs e)
+        {
+            SplashTimer.Stop();
+            Hide();
+            Program.Forms[(int)FormType.Start].Show();
         }
     }
 }
